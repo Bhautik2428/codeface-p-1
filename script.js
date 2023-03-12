f2dc4c34a1014728cdfd4f873aa421362642b492
bhavtikbariya2345@gmail.com <=>
2023-03-12 05:48:28 +0530

    100% responsive

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-var nav1inks = document.getElementById("nav1inks");
-console.log("nav1inks : ", nav1inks);
-function showmenu() {
-    console.log("something : ");
-    nav1inks.style.right = "0";
-}
-function hidemenu() {
-    console.log("hidemenu called: ");
-    nav1inks.style.right = "-270";
-}
+alert("welcome to Statue Of Unity");
